85fdbd26ed3fb33babeabac3ab1ccd1e56467200
Tim Coutinho <tmcoutinho42@gmail.com>
2017-05-17 01:00:47 -0700

    The product of the pythagorean triplet for which a + b + c = 1000
    Pretty slow and unoptimized.

---
+++ PE9.py
@@ -0,0 +1,13 @@
+def pythagTriplet(upto):
+    for i in range(1, upto + 1):
+        print(i)
+        for j in range(i, upto + 1):
+            if i + j > 1000:
+                break
+            for k in range(j, upto + 1):
+                if i + j + k > 1000:
+                    break
+                if i ** 2 + j ** 2 == k ** 2 and i + j + k == upto:
+                    return i * j * k
+
+print(pythagTriplet(1000))
